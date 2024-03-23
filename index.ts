d757448910026edd5605c5548458f2037d381186
UmmeSalma0 <UmmeSalma000@outlook.com>
2024-03-23 05:44:53 +0500

    I created a number guessing game.

---
+++ index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import iquirer from "inquirer";
+
+// computer generate a randomNumber
+// user guess the number
+// computer givefeedback
+
+const randomNumber = Math.floor(Math.random() * 10);
+
+const answers = await iquirer.prompt([
+  {
+    name: "userGuessNumber",
+    type: "number",
+    message: "please Guess a number between 1 to 10",
+  },
+]);
+console.log(answers);
+if (answers.userGuessNumber === randomNumber) {
+  console.log("you win");
+} else console.log("you lose");
